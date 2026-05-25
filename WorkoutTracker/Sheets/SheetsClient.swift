5f6be547d891b4ddcac53c288631d43dad244737
Sunny <git@freshpow.life>
2026-05-25 10:56:25 -0400

    feat: add SheetsClient protocol and error type

--- WorkoutTracker/Sheets/SheetsClient.swift
+++ WorkoutTracker/Sheets/SheetsClient.swift
@@ -1,5 +1,16 @@
 import Foundation
 
+protocol SheetsClient: Sendable {
+    func listTabTitles(spreadsheetId: String) async throws -> [String]
+    func fetchTab(spreadsheetId: String, tabName: String) async throws -> SheetGrid
+}
+
+enum SheetsError: Error, Equatable {
+    case notAuthorized
+    case http(Int)
+    case malformedResponse
+}
+
 func extractSpreadsheetId(from url: String) -> String? {
     guard let range = url.range(of: "/spreadsheets/d/") else { return nil }
     let rest = url[range.upperBound...]
